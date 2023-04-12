6acbc5c90e8053f1e017a6e49995cab1b1d080c6
vijender sharma <vijendersharma4711@gmailcom>
2023-04-12 11:29:22 +0530

    eslint config

--- src/Pages/components/StackCards.tsx
+++ src/Pages/components/StackCards.tsx
@@ -1,8 +1,5 @@
-import { useState } from "react";
 import classes from "./StackCard.module.css";
 const StackCards = () => {
-  const [animateClass, setAnimateClass] = useState("stack");
-
   return (
     <div className={classes.stack}>
       <div className={classes.stack__card}></div>
